73fd0d20c15c3d49b6f9d1ad4810d591abb7588e
Alessandro Martellone <a.martellone@gmail.com>
2014-01-29 15:35:02 +0100

    used augeas in dcrm scripts

--- deployment/puppet/dcrm/manifests/compute.pp
+++ deployment/puppet/dcrm/manifests/compute.pp
@@ -1,23 +1,18 @@
 class dcrm::compute {
 
-    exec { "echo \"# START PIVOT \n\" >> /etc/nova/nova.conf" :
-            subscribe=> Service["nova-api"],
-            path => '/bin'
-	}~>	
-    
-    exec { "echo \"pivot_address=127.0.1.1 \n\" >> /etc/nova/nova.conf" :
-	    path => '/bin'
-	}~>
+    augeas{ 'pivot_address':
+       context =>  "/files/etc/nova/nova.conf/.nova/",
+       changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler",
+    }->	
 
-    exec { "echo \"scheduler_ongoing_enabled=TRUE \n\" >> /etc/nova/nova.conf" :
-            path => '/bin'
-	}~>	
-	
-    exec { "echo \"scheduler_ongoing_tick=10 \n\" >> /etc/nova/nova.conf" :
-            path => '/bin'
-	}~>
-	
-    exec { "echo \"# STOP PIVOT \n\" >> /etc/nova/nova.conf" :
-	    path => '/bin'
-	}
+    augeas{ 'pivot_address':
+       context =>  "/files/etc/nova/nova.conf/.nova/",
+       changes =>  "set scheduler_ongoing_enabled TRUE",
+    }->	
+    
+    augeas{ 'pivot_address':
+       context =>  "/files/etc/nova/nova.conf/.nova/",
+       changes =>  "set scheduler_ongoing_tick 10",
+       subscribe=> Service["nova-api"],
+    }->	
 }
