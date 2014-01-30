dc9733830340de57aff22edcb7b95993b34de0b2
Alessandro Martellone <a.martellone@gmail.com>
2014-01-30 11:35:36 +0100

    disabled puppetdb

--- deployment/puppet/nailgun/manifests/init.pp
+++ deployment/puppet/nailgun/manifests/init.pp
@@ -58,7 +58,7 @@ class nailgun(
   Class["nailgun::nginx-nailgun"] ->
   Class["nailgun::cobbler"] ->
   Class["nailgun::pm"] ->
-  Class["nailgun::puppetdb"]->
+  #Class["nailgun::puppetdb"]->
   Class["openstack::logging"] ->
   Class["nailgun::supervisor"] ->
   Anchor<| title == "nailgun-end" |>
@@ -175,7 +175,7 @@ class nailgun(
     puppet_master_hostname => $puppet_master_hostname,
   }
 
-  class { "nailgun::puppetdb": }
+  #class { "nailgun::puppetdb": }
   class { "augeas": }
 
   class { "nailgun::mcollective":
