947ba0b5a29815a41e59b68d2fba537b8184019f
Alessandro M. Martellone <amartellone@ubuntu.(none)>
2014-02-03 05:29:28 -0800

    updated augeas references

--- deployment/puppet/dcrm/manifests/compute.pp
+++ deployment/puppet/dcrm/manifests/compute.pp
@@ -1,18 +1,19 @@
 class dcrm::compute {
-
-    augeas{ 'pivot_address':
-       context =>  "/files/etc/nova/nova.conf/.nova/",
-       changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler",
-    }->	
-
-    augeas{ 'pivot_address':
-       context =>  "/files/etc/nova/nova.conf/.nova/",
-       changes =>  "set scheduler_ongoing_enabled TRUE",
-    }->	
-    
-    augeas{ 'pivot_address':
-       context =>  "/files/etc/nova/nova.conf/.nova/",
-       changes =>  "set scheduler_ongoing_tick 10",
-       subscribe=> Service["nova-api"],
-    }->	
+    augeas { 'pivot_scheduler':
+	     context =>  "/files/etc/nova/nova.conf/.nova/",
+	     changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
+            }->
+    augeas  { 'nova_conf_scheduler_ongoing_enabled':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set scheduler_ongoing[last()+1] TRUE'
+	    }->
+    augeas  { 'nova_conf_pivot':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set pivot_address[last()+1] 127.0.0.1'
+	    }->
+    augeas  { 'nova_conf_scheduler_ongoing':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set scheduler_ongoing_enabled[last()+1] TRUE',
+	       subscribe=> Service["nova-api"]
+	    }
 }
--- deployment/puppet/dcrm/manifests/controller.pp
+++ deployment/puppet/dcrm/manifests/controller.pp
@@ -1,34 +1,27 @@
-    class dcrm::controller {
+class dcrm::controller {
     
-    exec { "update_novadb":
-		command => "python /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/manage.py upgrade ${sql_connection}",
-		path => "/usr/bin:/usr/sbin:/bin:/sbin",
-		onlyif => "test -f /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/versions/162_Add_instance_stats_table.pyc"
-	}
+    exec    { "update_novadb":
+		  command => "python /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/manage.py upgrade ${sql_connection}",
+		  path => "/usr/bin:/usr/sbin:/bin:/sbin",
+		  onlyif => "test -f /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/versions/162_Add_instance_stats_table.pyc"
+	    }
+    augeas { 'pivot_scheduler':
+	     context =>  "/files/etc/nova/nova.conf/.nova/",
+	     changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
+            }->
+    augeas  { 'nova_conf_scheduler_ongoing_enabled':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set scheduler_ongoing[last()+1] TRUE'
+	    }->
+    augeas  { 'nova_conf_pivot':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set pivot_address[last()+1] 127.0.0.1'
+	    }->
+    augeas  { 'nova_conf_scheduler_ongoing':
+	       context => '/files/etc/nova/nova.conf/.nova/',
+	       changes=> 'set scheduler_ongoing_enabled[last()+1] TRUE',
+	       subscribe=> Service["nova-api"]
+	    }
+}
     
-       augeas{ 'pivot_scheduler':
-	   context =>  "/files/etc/nova/nova.conf/.nova/",
-	   changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
-       }->
     
-       file { '/etc/nova/nova.conf':
-	     ensure => present,
-	     subscribe=> Service["nova-api"]
-	    } ->
-    
-       file_line { 'nova_conf_pivot_address':
-		  line => 'pivot_address=127.0.0.1',
-		  path => '/etc/nova/nova.conf'
-		 }->
-    
-       file_line { 'nova_conf_scheduler_ongoing':
-		 line => 'scheduler_ongoing_tick=10',
-		 path => '/etc/nova/nova.conf'
-		 }->
-    
-       file_line { 'nova_conf_scheduler_ongoing_enabled':
-		   line => 'scheduler_ongoing_enabled=TRUE',
-		   path=> '/etc/nova/nova.conf'
-		 }
-    
-    }
