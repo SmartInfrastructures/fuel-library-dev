2f3b2fe882d2b3c5b73a8efe296cec04d626eda2
Alessandro M. Martellone <a.martellone@gmail.com>
2014-02-04 05:04:57 -0800

    refined puppet script using augeas and file_line

--- deployment/puppet/dcrm/manifests/compute.pp
+++ deployment/puppet/dcrm/manifests/compute.pp
@@ -1,19 +1,28 @@
 class dcrm::compute {
-    augeas { 'pivot_scheduler':
-	     context =>  "/files/etc/nova/nova.conf/.nova/",
-	     changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
-            }->
-    augeas  { 'nova_conf_scheduler_ongoing_enabled':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set scheduler_ongoing[last()+1] TRUE'
-	    }->
-    augeas  { 'nova_conf_pivot':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set pivot_address[last()+1] 127.0.0.1'
-	    }->
-    augeas  { 'nova_conf_scheduler_ongoing':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set scheduler_ongoing_enabled[last()+1] TRUE',
-	       subscribe=> Service["nova-api"]
-	    }
+
+    augeas  { 'pivot_scheduler':
+             context =>  "/files/etc/nova/nova.conf/.nova/",
+             changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
+            }
+
+    file_line   { 'nova_conf_scheduler_ongoing_tick':
+                    line=> 'scheduler_ongoing_tick=10',
+                    path=> '/etc/nova/nova.conf'
+                }
+
+    file_line   { 'nova_conf_pivot':
+                     line => 'pivot_address=127.0.0.1',
+                     path => '/etc/nova/nova.conf'
+                }
+
+    file_line    { 'nova_conf_scheduler_ongoing_enabled':
+                    line => "scheduler_ongoing_enabled=TRUE",
+                    path => '/etc/nova/nova.conf'
+                }
+      
+    service     { 'nova-api':
+                    ensure => running,
+                    enable => true,
+                    subscribe => File['/etc/nova/nova.conf'],    
+                }
 }
--- deployment/puppet/dcrm/manifests/controller.pp
+++ deployment/puppet/dcrm/manifests/controller.pp
@@ -1,27 +1,37 @@
 class dcrm::controller {
-    
-    exec    { "update_novadb":
-		  command => "python /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/manage.py upgrade ${sql_connection}",
-		  path => "/usr/bin:/usr/sbin:/bin:/sbin",
-		  onlyif => "test -f /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/versions/162_Add_instance_stats_table.pyc"
-	    }
-    augeas { 'pivot_scheduler':
-	     context =>  "/files/etc/nova/nova.conf/.nova/",
-	     changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
-            }->
-    augeas  { 'nova_conf_scheduler_ongoing_enabled':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set scheduler_ongoing[last()+1] TRUE'
-	    }->
-    augeas  { 'nova_conf_pivot':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set pivot_address[last()+1] 127.0.0.1'
-	    }->
-    augeas  { 'nova_conf_scheduler_ongoing':
-	       context => '/files/etc/nova/nova.conf/.nova/',
-	       changes=> 'set scheduler_ongoing_enabled[last()+1] TRUE',
-	       subscribe=> Service["nova-api"]
-	    }
+
+    exec	{ "update_novadb":
+			command => "python /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/manage.py upgrade ${sql_connection}",
+			path => "/usr/bin:/usr/sbin:/bin:/sbin",
+			onlyif => "test -f /usr/lib/python2.7/dist-packages/nova/db/sqlalchemy/migrate_repo/versions/162_Add_instance_stats_table.pyc"
+	        }
+
+    augeas 	{ 'pivot_scheduler':
+			context =>  "/files/etc/nova/nova.conf/.nova/",
+		        changes =>  "set scheduler_driver nova.scheduler.pivot_scheduler.PivotScheduler"
+                }
+	    
+    file_line  { 'nova_conf_scheduler_ongoing_tick':
+			line=> 'scheduler_ongoing_tick=10',
+			path=> '/etc/nova/nova.conf'
+            }
+	    
+    file_line  { 'nova_conf_pivot':
+			line => 'pivot_address=127.0.0.1',
+			path => '/etc/nova/nova.conf'
+            }
+	    
+    file_line { 'nova_conf_scheduler_ongoing_enabled':
+			line => "scheduler_ongoing_enabled=TRUE",
+			path => '/etc/nova/nova.conf'
+        }
+	
+    service     { 'nova-api':
+                    ensure => running,
+                    enable => true,
+                    subscribe => File['/etc/nova/nova.conf'],    
+                }	
+
 }
-    
+
     
