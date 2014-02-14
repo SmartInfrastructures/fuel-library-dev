2df9defa11178bd5e8c7fbe922b3732108764ab3
Federico M. Facca <federico.facca@gmail.com>
2014-02-14 14:20:26 +0100

    updates to nailgun::puppetdb

--- deployment/puppet/nailgun/manifests/puppetdb.pp
+++ deployment/puppet/nailgun/manifests/puppetdb.pp
@@ -5,9 +5,20 @@ class nailgun::puppetdb(
   $puppet_service_name = 'puppetmaster'
 ){
 
+
+
+  class { 'puppetdb::master::routes':
+      puppet_confdir => $puppet_confdir
+  }
+
+  class { 'puppetdb::master::storeconfigs':
+      puppet_conf => $puppet_conf,
+  }
+
   class { 'puppetdb::server':
     listen_port => $listen_port,
     database => 'embedded',
+    puppetdb_version => 'latest',
   }
   
   firewall { '100 allow pupptedb access':
@@ -15,15 +26,15 @@ class nailgun::puppetdb(
     proto  => tcp,
     action => accept,
   }
- 
-  class { 'puppetdb::master::routes':
-      puppet_confdir => $puppet_confdir
-  }
-   
-  class { 'puppetdb::master::storeconfigs':
-      puppet_conf => $puppet_conf,
-  }
+  
+  Package['puppetdb'] ->
+  Class['puppetdb::server::firewall'] ->
+  Class['puppetdb::server::database_ini'] ->
+  Class['puppetdb::server::jetty_ini'] ->
+  Exec['puppetdb-ssl-setup'] ->
+  Service['puppetdb']
 
- Class['puppetdb::master::routes']        ~> Service[$puppet_service_name]
-
+  Class['puppetdb::master::routes']->
+  Class['puppetdb::master::storeconfigs']->
+  Class['puppetdb::server']~> Service[$puppet_service_name]
 }
