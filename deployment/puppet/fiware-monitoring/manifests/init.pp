9f26b1617e7be5e4651fb33a0eacffd8ad9f8e09
Alessandro M. Martellone <a.martellone@gmail.com>
2014-02-17 01:17:37 -0800

    updated fiware-monitoring script

--- deployment/puppet/fiware-monitoring/manifests/init.pp
+++ deployment/puppet/fiware-monitoring/manifests/init.pp
@@ -2,24 +2,21 @@ class fiware-monitoring {
 	
 	notify { "fiware-monitoring_message":
 		message => "FIWARE-Monitoring installation"
-	}
-	file {  "~/fiware-monitoring/":
+	}->
+	file {  "/home/fiware-monitoring/":
               ensure => "directory",
               mode => 755
             }->
      	file {  "coping_fiware-monitoring":
               source => "puppet:///modules/fiware-monitoring/fiware-monitoring.tar.gz",
-              path => "~/fiware-monitoring/fiware-monitoring.tar.gz",
+              path => "/home/fiware-monitoring/fiware-monitoring.tar.gz",
               recurse => true,
               mode => 0755
             }->
 	exec {  "untar_fiware-monitoring":
-              command => "tar -xf ~/fiware-monitoring/fiware-monitoring.tar.gz",
-              cwd => "~/fiware-monitoring",
+              command => "tar -xf /home/fiware-monitoring/fiware-monitoring.tar.gz",
+              cwd => /home/fiware-monitoring",
               path => "/bin:/usr/bin"
-            }->
-	exec {  "install_ngsi_adapter":
-              command => "npm install",
-              path => "/usr/bin:/usr/sbin:/bin:/sbin",
             }
+
 }         
