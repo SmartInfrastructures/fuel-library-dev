c957839f69d4f6aa601fc47b4762cb93cc0b4dad
Alessandro M. Martellone <a.martellone@gmail.com>
2014-02-17 02:53:22 -0800

    updated puppet script

--- deployment/puppet/fiware-monitoring/manifests/init.pp
+++ deployment/puppet/fiware-monitoring/manifests/init.pp
@@ -17,6 +17,20 @@ class fiware-monitoring {
               command => "tar -xf /home/fiware-monitoring/fiware-monitoring.tar.gz",
               cwd => /home/fiware-monitoring",
               path => "/bin:/usr/bin"
-            }
+            }->
+	file {  "fiware_odc_region_copy":
+              source => "puppet:///modules/fiware-monitoring/region.js",
+              path => "/home/fiware-monitoring/ngsi_adapter/lib/parser/region.js",
+              recurse => true,
+              mode => 0755
+            }->
+	exec { "npm_install_ngsi_adapter":
+   	      command => "/usr/bin/npm install /home/fiware-monitoring/ngsi_adapter",
+   	      path    => "/usr/local/bin/:/bin/:/usr/bin/npm",
+	}->
+	exec { "run_fiware-monitoring":
+   	      command => "nohup /home/fiware-monitoring/ngsi_adapter/adapter --listenPort 1337 --brokerUrl http://localhost:1026 &",
+   	      path    => "/usr/local/bin/:/bin/",
+	}
 
 }         
