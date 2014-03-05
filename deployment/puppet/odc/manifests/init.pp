6d057ebb4b0188cbe77bc3314c284db4749090da
amartellone <a.martellone@gmail.com>
2014-03-05 16:13:18 +0100

    modified the passage of variables

--- deployment/puppet/odc/manifests/init.pp
+++ deployment/puppet/odc/manifests/init.pp
@@ -1,16 +1,15 @@
 class odc (
-	  $username = '',
-	  $password = '',
-	  $tenant_name = '',
-	  $auth_url = '',
-	  $token = '',
-	  $region_name = '',
-	  $region_id = '',
-	  $location = '',
-	  $latitude = '',
-	  $longitude = '',
-	  $agent_url = '')
-	 {
+	  $username = 'changeit',
+	  $password = 'changeit',
+	  $tenant_name = 'changeit',
+	  $auth_url = 'changeit',
+	  $token = 'changeit',
+	  $region_name = 'changeit',
+	  $region_id = 'changeit',
+	  $location = 'changeit',
+	  $latitude = '42.00',
+	  $longitude = '11.00',
+	  $agent_url = 'changeit') {
 	
 	notify { "odc_message":
 		message => "OpenStackDataCollector installation"
@@ -36,51 +35,62 @@ class odc (
               path => "/home/osdatacollector/odc.conf",
               recurse => true,
               mode => 0777
-            }->
+            }
 	augeas 	{ 'update_file_odc_1':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set username ${username}"
-                }->
+		        changes =>  "set username ${username}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_2':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set password ${password}"
-                }->
+		        changes =>  "set password ${password}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_3':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set tenant_name ${tenant_name}"
-                }->
+		        changes =>  "set tenant_name ${tenant_name}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_4':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set auth_url ${auth_url}"
-                }->
+		        changes =>  "set auth_url ${auth_url}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_5':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set token ${token}"
-                }->
+		        changes =>  "set token ${token}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_6':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set regionName ${region_name}"
-                }->
+		        changes =>  "set regionName ${region_name}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_7':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set regionId ${region_id}"
-                }->
+		        changes =>  "set regionId ${region_id}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_8':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set location ${location}"
-                }->
+		        changes =>  "set location ${location}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_9':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set latitude ${latitude}"
-                }->
+		        changes =>  "set latitude ${latitude}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_10':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set longitude ${longitude}"
-                }->
+		        changes =>  "set longitude ${longitude}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	augeas 	{ 'update_file_odc_11':
 			context =>  "/files/home/osdatacollector/odc.conf/.odc/",
-		        changes =>  "set agentUrl ${agent_url}"
-                }->
+		        changes =>  "set agentUrl ${agent_url}",
+		        onlyif => "test -f /home/osdatacollector/odc.conf"
+                }
 	exec { "run_osd":
    	      command => "/usr/bin/python /home/osdatacollector/openstackDataCollector.py &",
    	      path    => "/usr/local/bin/:/bin/:/usr/bin/python",
