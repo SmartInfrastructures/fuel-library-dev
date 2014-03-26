218ad17c87484161ffedf9a394c915f20bf377e1
Alessandro M. Martellone <a.martellone@gmail.com>
2014-03-26 02:59:22 -0700

    added CB installation

--- deployment/puppet/context-broker/manifests/init.pp
+++ deployment/puppet/context-broker/manifests/init.pp
@@ -3,8 +3,23 @@ class context-broker {
 	notify { "cb_message":
 		message => "ContextBroker installation"
 	}
- 	package { "contextbroker":
-    		ensure => "installed"
-	}
+ 	file {  "/home/context-broker/":
+              ensure => "directory",
+              mode => 755
+            }->
+	file {  "cb_copy":
+              source => "puppet:///modules/context-broker/contextbroker_0.9.1-2_amd64.deb",
+              path => "/home/context-broker/contextbroker_0.9.1-2_amd64.deb",
+              recurse => true,
+              mode => 0755
+            }->
+	exec {  "cb_install":
+              command => "dpkg -i /home/context-broker/contextbroker_0.9.1-2_amd64.deb",
+              path => "/bin:/usr/bin:/usr/sbin:/sbin"
+          }
+	#->
+	#exec { "run_cb":
+   	#      command => "contextBroker",
+   	#      path    => "/usr/local/bin/:/bin/:/usr/bin/python",
+	#}
 }         
-
