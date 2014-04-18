5c5ffebc716b815dcae5a5594387fae59d3b7be5
amartellone <a.martellone@gmail.com>
2014-04-18 13:25:25 +0200

    Update install_libmicrohttpd.sh

--- deployment/puppet/context-broker/files/install_libmicrohttpd.sh
+++ deployment/puppet/context-broker/files/install_libmicrohttpd.sh
@@ -3,6 +3,6 @@ cd /home/context-broker/libmicrohttpd-0.9.16/
 ./configure
 make
 make install
-if [ ! -L 'usr/lib/libmicrohttpd.so.10' ]; then
+if [ ! -L '/usr/lib/libmicrohttpd.so.10' ]; then
 	ln -s /usr/local/lib/libmicrohttpd.so.10 /usr/lib/libmicrohttpd.so.10
 fi
