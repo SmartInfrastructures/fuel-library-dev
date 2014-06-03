f26562132a34eb84005b89d260d95a434f32855b
Alessandro M. Martellone <a.martellone@gmail.com>
2014-06-03 16:17:58 +0200

    nagios configuration fixes

--- deployment/puppet/nagios/files/fix_and_run.sh
+++ deployment/puppet/nagios/files/fix_and_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sed -i 's/[^_]glance-         registry/glance-registry\ncheck_command\t\tcheck_http_api!9191\n/g' /etc/nagios*/xifi-monitoring_master/node-*_services.cfg
+sed -i 's/[^_]glance-registry/glance-registry\ncheck_command\t\tcheck_http_api!9191\n/g' /etc/nagios*/xifi-monitoring_master/node-*_services.cfg
+chown root:nagios /usr/lib/nagios/plugins/check_*
+chmod u+s /usr/lib/nagios/plugins/check_*
 /etc/init.d/nagios3 restart
