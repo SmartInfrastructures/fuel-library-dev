8575d36a06f2b13dfc06925e5cca656141e06691
amartellone <a.martellone@gmail.com>
2014-07-17 14:14:13 +0200

    Update init.pp

--- deployment/puppet/nagios/manifests/init.pp
+++ deployment/puppet/nagios/manifests/init.pp
@@ -22,7 +22,7 @@ $nrpeservice       = $nagios::params::nrpeservice,
       $basic_services = ['yum','kernel','libs','load','procs','zombie','swap','user','cpu','memory']   
     }
     'Debian': {
-      $basic_services = ['load','procs','zombie','swap','user','cpu','memory']
+      $basic_services = ['procs','zombie','swap','user','cpu-load','memory']
       
       #temp - we will fix	the iso	;)
       apt::source { 'precise_nagios':
